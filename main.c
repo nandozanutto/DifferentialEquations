e2dff52f1627a1d9a5860d1447531889461d5f65
nandozanutto <nando.zanutto@gmail.com>
2021-06-06 01:18:57 -0300

    Part1 done

--- main.c
+++ main.c
@@ -4,8 +4,7 @@
 
 #define MAXIT   50  // Número máximo de iterações em métodos iterativos
 
-//y'' = 6x - 0.5x², x e (0,12)
-//y(0)=0 y(12) = 0
+
 
 typedef float real_t;
 
@@ -17,6 +16,18 @@ typedef struct {
     double (* p)(double), (* q)(double), (* r)(double);
 }Edo;
 
+double getP(double x){
+    return 0.0;
+}
+
+double getQ(double x){
+    return 0.0;
+}
+
+double getR(double x){//Eq a.
+    //6x - 0.5x²
+    return 6*x - 0.5*x*x;
+}
 
 void gaussSeidel(Edo *edoeq, double *Y)
 {
@@ -46,19 +57,23 @@ void gaussSeidel(Edo *edoeq, double *Y)
 
 
 int main(){
-    int n=5;
-    int a=0, b=12;
-    real_t malha[n+2];
-    real_t h;
-    h = (b-a)/(n+1);
-    malha[0] = a;
-    malha[n+1] = b;
-    for(int i=1; i<n+1; i++)
-        malha[i] = a + i*h;
+
+    Edo *edoeq = (Edo *)malloc(sizeof(Edo));
+    //y'' = 6x - 0.5x², x e (0,12)
+    //y(0)=0 y(12) = 0
+    edoeq->a = 0;
+    edoeq->b = 12;
+    edoeq->n = 5;
+    edoeq->ya = 0;
+    edoeq->yb = 0;
+    edoeq->p = getP;
+    edoeq->q = getQ;
+    edoeq->r = getR;
+    double *Y;
+    Y = (double *)calloc((edoeq->n), sizeof(double));
+    gaussSeidel(edoeq, Y);
+    for(int i=0; i<edoeq->n; i++)
+        printf("%f ", Y[i]);
     
-    // for(int i=0; i<n+2; i++)
-    //     printf("%f\n", malha[i]);
-
-
-
+    
 }
